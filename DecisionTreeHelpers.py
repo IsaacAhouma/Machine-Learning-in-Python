6da1b8301ecf5665f42c6e6ba05906446fcbd7ec
Isaac Ahouma <ivorianboy@gmail.com>
2016-06-03 08:44:11 -0700

    Update DecisionTreeHelpers.py

--- DecisionTreeHelpers.py
+++ DecisionTreeHelpers.py
@@ -1,3 +1,10 @@
+
+"""
+Created on Wed Jun 01 12:49:25 2016
+
+@author: Isaac
+"""
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 31 10:30:20 2016
@@ -7,7 +14,7 @@ Created on Tue May 31 10:30:20 2016
 """
 import graphlab
 # Method 1
-target='safe_loan'
+#target='safe_loans'
 def intermediate_node_num_mistakes(labels_in_node):
     # Corner case: If labels_in_node is empty, return 0
     if len(labels_in_node) == 0:
@@ -30,29 +37,6 @@ def intermediate_node_num_mistakes(labels_in_node):
         majority_class=count_neg
         minority_class=count_pos      
     return minority_class
-
-# Tests for Method 1   
-# Test case 1
-example_labels = graphlab.SArray([-1, -1, 1, 1, 1])
-if intermediate_node_num_mistakes(example_labels) == 2:
-    print ('Test passed!')
-else:
-    print ('Test 1 failed... try again!')
-
-# Test case 2
-example_labels = graphlab.SArray([-1, -1, 1, 1, 1, 1, 1])
-if intermediate_node_num_mistakes(example_labels) == 2:
-    print ('Test passed!')
-else:
-    print ('Test 2 failed... try again!')
-    
-# Test case 3
-example_labels = graphlab.SArray([-1, -1, -1, -1, -1, 1, 1])
-if intermediate_node_num_mistakes(example_labels) == 2:
-    print ('Test passed!')
-else:
-    print ('Test 3 failed... try again!')
-
 
 def best_splitting_feature(data, features, target):
     
@@ -95,52 +79,81 @@ def create_leaf(target_values):
     # Return the leaf node        
     return leaf
     
-def decision_tree_create(data, features, target, current_depth = 0, max_depth = 10):
-    remaining_features = features[:] # Making a copy of the features
+def decision_tree_create(data, features, target, current_depth = 0, 
+                         max_depth = 10, min_node_size=1, 
+                         min_error_reduction=0.0):
+    
+    remaining_features = features[:] # Make a copy of the features.
+    
     target_values = data[target]
-    print ("--------------------------------------------------------------------")
+    print "--------------------------------------------------------------------"
     print "Subtree, depth = %s (%s data points)." % (current_depth, len(target_values))
-    # Stopping condition 1
-    # (Check if there are mistakes at current node.
-    # Recall you wrote a function intermediate_node_num_mistakes to compute this.)
+    
+    
+    # Stopping condition 1: All nodes are of the same type.
     if intermediate_node_num_mistakes(target_values) == 0:
-        print "Stopping condition 1 reached."     
-        # If no mistakes at current node, make current node a leaf node
+        print "Stopping condition 1 reached. All data points have the same target value."                
         return create_leaf(target_values)
-    # Stopping condition 2 (check if there are remaining features to consider splitting on)
-    if remaining_features == [] :
-        print "Stopping condition 2 reached."    
-        # If there are no remaining features to consider, make current node a leaf node
+    
+    # Stopping condition 2: No more features to split on.
+    if remaining_features == []:
+        print "Stopping condition 2 reached. No remaining features."                
         return create_leaf(target_values)    
-    # Additional stopping condition (limit tree depth)
+    
+    # Early stopping condition 1: Reached max depth limit.
     if current_depth >= max_depth:
-        print "Reached maximum depth. Stopping for now."
-        # If the max tree depth has been reached, make current node a leaf node
+        print "Early stopping condition 1 reached. Reached maximum depth."
         return create_leaf(target_values)
-    # Find the best splitting feature (recall the function best_splitting_feature implemented above)
-    splitting_feature=best_splitting_feature(data,remaining_features,target)  
-    # Split on the best feature
+    
+    # Early stopping condition 2: Reached the minimum node size.
+    # If the number of data points is less than or equal to the minimum size, return a leaf.
+    if reached_minimum_node_size(data, min_node_size) is True: ## YOUR CODE HERE 
+        print "Early stopping condition 2 reached. Reached minimum node size."
+        return create_leaf(target_values)  ## YOUR CODE HERE
+    
+    # Find the best splitting feature
+    splitting_feature = best_splitting_feature(data, features, target)
+    
+    # Split on the best feature that we found. 
     left_split = data[data[splitting_feature] == 0]
     right_split = data[data[splitting_feature] == 1]
+    
+    # Early stopping condition 3: Minimum error reduction
+    # Calculate the error before splitting (number of misclassified examples 
+    # divided by the total number of examples)
+    error_before_split = intermediate_node_num_mistakes(target_values) / float(len(data))
+    
+    # Calculate the error after splitting (number of misclassified examples 
+    # in both groups divided by the total number of examples)
+    left_mistakes = intermediate_node_num_mistakes(left_split[target])   ## YOUR CODE HERE
+    right_mistakes = intermediate_node_num_mistakes(right_split[target])  ## YOUR CODE HERE
+    error_after_split = (left_mistakes + right_mistakes) / float(len(data))
+    
+    # If the error reduction is LESS THAN OR EQUAL TO min_error_reduction, return a leaf.
+    if error_reduction(error_before_split, error_after_split) <= min_error_reduction:        ## YOUR CODE HERE
+        print "Early stopping condition 3 reached. Minimum error reduction."
+        return create_leaf(target_values)  ## YOUR CODE HERE 
+    
+    
     remaining_features.remove(splitting_feature)
     print "Split on feature %s. (%s, %s)" % (\
-                      splitting_feature, len(left_split), len(right_split)) 
-    # Create a leaf node if the split is "perfect"
-    if len(left_split) == len(data):
-        print "Creating leaf node."
-        return create_leaf(left_split[target])
-    if len(right_split) == len(data):
-        print "Creating leaf node."
-        create_leaf(target_values)     
+                      splitting_feature, len(left_split), len(right_split))
+    
+    
     # Repeat (recurse) on left and right subtrees
-    left_tree = decision_tree_create(left_split, remaining_features, target, current_depth + 1, max_depth)        
-    right_tree = decision_tree_create(right_split, remaining_features, target, current_depth + 1, max_depth) 
-
+    left_tree = decision_tree_create(left_split, remaining_features, target, 
+                                     current_depth + 1, max_depth, min_node_size, min_error_reduction)        
+    
+    ## YOUR CODE HERE
+    right_tree = decision_tree_create(right_split, remaining_features, target, 
+                                      current_depth + 1, max_depth, min_node_size, min_error_reduction)        
+    
     return {'is_leaf'          : False, 
             'prediction'       : None,
             'splitting_feature': splitting_feature,
             'left'             : left_tree, 
             'right'            : right_tree}
+
 
 def count_nodes(tree):
     if tree['is_leaf']:
@@ -167,7 +180,7 @@ def evaluate_classification_error(tree, data):
     # Apply the classify(tree, x) to each row in your data
     prediction = data.apply(lambda x: classify(tree, x)) 
     #calculate the classification error and return it
-    return sum(prediction==data[target])/float(len(data))
+    return sum(prediction==data['safe_loans'])/float(len(data))
 
 
 def print_stump(tree, name = 'root'):
@@ -190,4 +203,13 @@ def print_stump(tree, name = 'root'):
            ('leaf, label: ' + str(tree['right']['prediction']) if tree['right']['is_leaf'] else 'subtree'))
 
 
-    
+def reached_minimum_node_size(data, min_node_size):
+    return data <= min_node_size
+
+def error_reduction(error_before_split, error_after_split):
+    return error_before_split-error_after_split
+
+def count_leaves(tree):
+    if tree['is_leaf']:
+        return 1
+    return count_leaves(tree['left']) + count_leaves(tree['right'])
